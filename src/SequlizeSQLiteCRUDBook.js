ec3fc7b11ddeab7f43fa0c83a13a2f07a3680330
Ryu6406021610101 <s6406021610101@email.kmutnb.ac.th>
2025-02-11 09:05:12 +0700

    sycn

---
+++ src/SequlizeSQLiteCRUDBook.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const Sequelize = require('sequelize');
+const app = express();
+
+app.use(express.json());
+
+const sequelize = new Sequelize('database', 'username', 'password', {
+    host: 'localhost',
+    dialect: 'sqlite',
+    storage: './Database/SQBooks.sqlite'
+});
+
+const Book = sequelize.define('book', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    title: {
+        tye: Sequelize.STRING,
+        all
+    }
+})
